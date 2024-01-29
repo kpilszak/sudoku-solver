0fa693c7a649e2636c546385805e8888f5c0009b
kpilszak <klaudia.pilszak@gmail.com>
2024-01-29 15:58:13 -0500

    Join input values when solve button clicked

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const puzzleBoard = document.querySelector('#puzzle')
 const solveButton = document.querySelector('#solve-button')
 const squares = 81
+const submission = []
 
 for (let i = 0; i < squares; i++) {
     const inputElement = document.createElement('input')
@@ -9,3 +10,17 @@ for (let i = 0; i < squares; i++) {
     inputElement.setAttribute('max', '9')
     puzzleBoard.appendChild(inputElement)
 }
+
+
+const joinValues = () => {
+    const inputs = document.querySelectorAll('input')
+    inputs.forEach(input => {
+        if (input.value) {
+            submission.push(input.value)
+        } else {
+            submission.push('.')
+        }
+    })
+}
+
+solveButton.addEventListener('click', joinValues)
