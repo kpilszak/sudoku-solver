a252ce92aa07db2d0d62ec74d85b3d06f5032395
kpilszak <klaudia.pilszak@gmail.com>
2024-01-31 18:16:01 -0500

    Style board

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ for (let i = 0; i < squares; i++) {
     inputElement.setAttribute('type', 'number')
     inputElement.setAttribute('min', '0')
     inputElement.setAttribute('max', '9')
+
+    if (
+        ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i < 21) ||
+        ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i < 27) ||
+        ((i % 9 == 3 || i % 9 == 4 || i % 9 == 5) && (i > 27 && i < 53)) ||
+        ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i > 53) ||
+        ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i > 53)
+    ) {
+        inputElement.classList.add('odd-section')
+    }
+
     puzzleBoard.appendChild(inputElement)
 }
 
